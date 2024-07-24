96ecc2a9d2c02447870cb9de35418a7ea91811c4
andhika2229 <fahreziandhika59d@gmail.com>
2024-07-24 16:55:00 +0700

    Add files via upload

---
+++ html.com/main.js
@@ -0,0 +1,9 @@
+
+    function gamau(id) {
+        var mau = document.getElementById("btn_mau");
+        var i = Math.floor(Math.random()*300)+1;
+        var j = Math.floor(Math.random()*100)+mau.offsetTop;
+        id.style.left = i+"px";
+        id.style.top = j+"px";
+    }
+    alert("HAII KAMU DI LIAT YA INI BUATAN AKU SENDIRI LOH")
